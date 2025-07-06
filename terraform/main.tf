fa6b3f10b8a48a1d3f246c07542d7d85574ee858
Franco du Buisson <franco.dubuisson@bbd.co.za>
2025-07-06 14:20:09 +0200

    Setup AWS deployment pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {        
+  backend "s3" {
     region = var.region_name
   }
 }
 
 provider "aws" {
-  region =  var.region_name
+  region = var.region_name
 }
 
 resource "aws_default_vpc" "default_vpc" {
@@ -22,7 +21,6 @@ resource "aws_default_vpc" "default_vpc" {
 }
 
 data "aws_availability_zones" "available_zones" {
-  
 }
 
 resource "aws_default_subnet" "subnet_az1" {
@@ -35,14 +33,14 @@ resource "aws_default_subnet" "subnet_az2" {
 
 resource "aws_security_group" "allow_postgres" {
   name_prefix = "allow_postgres_"
-
-    ingress {
-        from_port   = 5432
-        to_port     = 5432
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
 data "aws_secretsmanager_secret_version" "postgresuser" {
   secret_id = "postgresuser"
@@ -71,27 +69,27 @@ resource "aws_db_instance" "peardb" {
 }
 
 output "db_host" {
-  value = aws_db_instance.peardb.endpoint
+  value       = aws_db_instance.peardb.endpoint
   description = "The endpoint of the Postgres Server RDS instance"
 }
 
 resource "aws_security_group" "ec2_security_group" {
   name_prefix = "pear_api_sg"
-
+  
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   ingress {
     from_port   = 443
     to_port     = 443
@@ -107,113 +105,136 @@ resource "aws_security_group" "ec2_security_group" {
   }
 }
 
-resource "aws_instance" "pear_ec2_instance" {
+# Separate EC2 instance for API
+resource "aws_instance" "pear_api_ec2_instance" {
   ami           = "ami-0b7e05c6022fc830b"
   instance_type = "t3.micro"
-  key_name      = "pear-key"
+  key_name      = "pear-api-key"
   tags = {
-    Name = "pear_ec2_instance"
+    Name = "pear_api_ec2_instance"
   }
-
-  vpc_security_group_ids = [ aws_security_group.ec2_security_group.id ]
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 }
 
-# resource "aws_budgets_budget" "pear_budget" {
-#   name              = "pear_budget"
-#   budget_type       = "COST"
-#   limit_amount      = "50"
-#   limit_unit        = "USD"
-#   time_period_end   = "2025-07-11_00:00"
-#   time_period_start = "2025-07-01_00:00"
-#   time_unit         = "MONTHLY"
+# Separate EC2 instance for Web/Frontend
+resource "aws_instance" "pear_web_ec2_instance" {
+  ami           = "ami-0b7e05c6022fc830b"
+  instance_type = "t3.micro"
+  key_name      = "pear-web-key"
+  tags = {
+    Name = "pear_web_ec2_instance"
+  }
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+}
 
-#   notification {
-#     comparison_operator        = "EQUAL_TO"
-#     threshold                  = 50
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "FORECASTED"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
+resource "aws_budgets_budget" "pear_budget" {
+  name              = "pear_budget"
+  budget_type       = "COST"
+  limit_amount      = "25"
+  limit_unit        = "USD"
+  time_period_end   = "2025-07-11_00:00"
+  time_period_start = "2025-07-01_00:00"
+  time_unit         = "MONTHLY"
+  
+  notification {
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "EQUAL_TO"
+    threshold                  = 75
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 10
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 20
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 30
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 40
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 60
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+  
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 90
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.budget_notification_emails
+  }
+}
 
-#   notification {
-#     comparison_operator        = "EQUAL_TO"
-#     threshold                  = 75
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "FORECASTED"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 10
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 20
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 30
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 40
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 50
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 60
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 80
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-
-#   notification {
-#     comparison_operator        = "GREATER_THAN"
-#     threshold                  = 90
-#     threshold_type             = "PERCENTAGE"
-#     notification_type          = "ACTUAL"
-#     subscriber_email_addresses = var.budget_notification_emails
-#   }
-# }
-
-resource "aws_eip" "pear_ec2_eip" {
-  instance = aws_instance.pear_ec2_instance.id
+# Elastic IPs for both instances
+resource "aws_eip" "pear_api_ec2_eip" {
+  instance = aws_instance.pear_api_ec2_instance.id
   domain   = "vpc"
 }
 
-output "ec2_host" {
-  value = aws_eip.pear_ec2_eip.public_dns
-  description = "The endpoint of the EC2 instance"
+resource "aws_eip" "pear_web_ec2_eip" {
+  instance = aws_instance.pear_web_ec2_instance.id
+  domain   = "vpc"
+}
+
+# Outputs for both instances
+output "api_ec2_host" {
+  value       = aws_eip.pear_api_ec2_eip.public_dns
+  description = "The endpoint of the EC2 instance for API"
+}
+
+output "web_ec2_host" {
+  value       = aws_eip.pear_web_ec2_eip.public_dns
+  description = "The endpoint of the EC2 instance for WebApp"
 }
